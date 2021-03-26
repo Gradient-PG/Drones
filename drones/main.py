3810812746c78b25554d66d0f651ba176c100b06
Qubaef <qubaefg@gmail.com>
2021-03-26 13:49:08 +0100

    add 'connection' module; add main file; setup logging
    Add 'connection' module folder with basic interface and usage examples.
    Add main file with basic logging config, which is available for all the modules.

---
+++ drones/main.py
@@ -0,0 +1,25 @@
+"""Main project file.
+
+It's where the API entrypoints are accessed and used for the purpose of the project.
+"""
+
+import logging
+from connection import c  # Sample library import
+
+
+def setup_log():
+    """Setup global log config."""
+
+    logging.basicConfig(
+        filename="logfile.log",
+        filemode="w",
+        level=logging.DEBUG,
+        format="[%(filename)s] [%(levelname)s] : %(message)s",
+    )
+    logging.info("Log initialized")
+
+
+if __name__ == "__main__":
+    setup_log()
+    c.connect()  # Sample entrypoint call
+    # c.__track_errors()    # Sample invalid call - function inaccessible
