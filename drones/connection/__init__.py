83edc4d9e29c9b93ce5635a5581e89e091d714b1
Qubaef <qubaefg@gmail.com>
2021-03-26 13:49:08 +0100

    add 'connection' module; add main file; setup logging
    Add 'connection' module folder with basic interface and usage examples.
    Add main file with basic logging config, which is available for all the modules.

---
+++ drones/connection/__init__.py
@@ -0,0 +1,9 @@
+"""Library to manage communication with Tello drone.
+
+Additional info.
+"""
+
+# Import prototype and share it as global object
+from drones.connection.prototype import ConnectorPrototype
+
+c = ConnectorPrototype()
