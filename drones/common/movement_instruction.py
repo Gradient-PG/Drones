20606eb1732e9c4711183f82e231fb360a66b162
Qubaef <52603374+Qubaef@users.noreply.github.com>
2021-04-15 18:48:02 +0200

    add template of connector; add common classes (#7)
    * add template of connector; add common classes
    
    Add template of connector specifying the agreed architecture.
    Add basic forms of common classes, which will be shared between modules.

---
+++ drones/common/movement_instruction.py
@@ -0,0 +1,25 @@
+"""Movement instruction bundle."""
+
+from dataclasses import dataclass
+from typing import List
+
+import drone_instruction as di
+
+
+@dataclass
+class MovementInstruction:
+    """Movement instruction bundle
+
+    To translate bundle into drone's instructions, use instruction.translate()
+    """
+
+    target_yaw: int
+    diff_x: int
+    diff_y: int
+    diff_z: int
+    speed: int
+
+    def translate(self) -> List[str]:
+        """Translate movement instruction to list of drone instructions"""
+
+        return [di.command(), di.takeoff(), di.land()]
