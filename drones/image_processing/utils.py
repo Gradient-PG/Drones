6d7ed96a0c74fb04ac971751ee5fe75790ce9c48
MBoshy <bosakmich@gmail.com>
2021-04-18 19:55:48 +0200

    determining distance between from camera and vector from object to centre

--- drones/image_processing/utils.py
+++ drones/image_processing/utils.py
@@ -11,7 +11,7 @@ def calculate_focal(knownWidth: float, knownDistance: float, pixelWidth: int) ->
         measured distance between detected object and camera (has to be known before running script)
     pixelWidth: int
         width in pixels of detected object
-        
+
     Returns:
     ----------
     focalLength: float
@@ -58,3 +58,34 @@ def distance_to_camera(knownWidth: float, focalLength: float, pixelWidth: int) -
         raise ZeroDivisionError(err)
 
     return distance
+
+
+def centre_vector(frameWidth: int, frameHeight: int, objectCoordinates: tuple, centreHeightCoeff: float) -> tuple:
+    """
+    Calculate vector from tracked object to center of frame
+
+    Parameters:
+    ----------
+    frameWidth: int
+        width of frame captured from camera
+    frameHeight: int
+        height of frame captured from camera
+    objectCoordinates: tuple
+        coordinates (x, y) of tracked object's center
+    centreHeightCoeff: float
+        coefficient defining height of center where tracked object should be
+
+    Returns:
+    ----------
+    vector_to_centre: tuple
+        vector (x, y) from tracked object to center of frame
+    """
+
+    xCentre = 0.5 * frameWidth
+    yCentre = centreHeightCoeff * frameHeight
+
+    xComponent = xCentre - objectCoordinates[0]
+    yComponent = yCentre - objectCoordinates[1]
+    vectorToCentre = (xComponent, yComponent)
+
+    return vectorToCentre
