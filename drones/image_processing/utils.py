586dde0ebd9c13325ea28d857369e5174a716daa
uraes <michal.siecz04@gmail.com>
2021-04-21 21:08:13 +0200

    Return center of detecting object; Add config; Minor code style changes

--- drones/image_processing/utils.py
+++ drones/image_processing/utils.py
@@ -1,31 +1,38 @@
 import numpy as np
 import cv2 as cv
 import imutils
+import configparser
+import typing
 import drones.image_processing.normalization as normalization
 
-# These constants indicate color range in HSV of object to be detected.
-LOWER_BOUND_COLOR = np.array([0, 220, 100])
-UPPER_BOUND_COLOR = np.array([15, 255, 255])
+
+config_parser = configparser.ConfigParser()
+config_parser.read("drones/image_processing/config.ini")
+config = config_parser["COLOR_RANGE"]
 
 
-def detect_object(image: np.ndarray) -> int:
+def detect_object(image: np.ndarray) -> typing.Tuple[typing.Tuple[int, int], int]:
     """Detect object in the image.
 
     The detection is basicaly applying mask in specific color range
     and finding contour with best circularity and area combined.
     Next the minimal enclosing circle of the chosen contour is evaluated
-    and its diameter returned.
+    and its diameter and center returned.
 
-    Parameters
+    Parameters:
     ----------
-    image : np.ndarray
+    image: np.ndarray
         Image in which the object should be detected.
 
-    Returns
+    Returns:
     -------
-    int
-        Diameter of minimal enclosing circle of detected object in pixels.
-        If the object is not detected 0 is returned.
+    center_and_diameter: typing.Tuple[typing.Tuple[int,int],int]
+        The inside tuple is x and y coordinates on the picture
+        of center of minimal enclosing circle of detected object.
+        The other value is the diameter of minimal enclosing circle of detected object in pixels.
+        If the object is not detected tuple ((-1,-1),-1) is returned
+        (so center coordinates and diameter are -1).
+        This returned format is insired by OpenCV minEnclosingCircle function returned format.
     """
     # Image normalization to make colors more visious and less light vulnerable.
     image = normalization.normalization(image, minmax=True, clahe=True)
@@ -34,7 +41,11 @@ def detect_object(image: np.ndarray) -> int:
     hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
     # Mask in given color range.
-    mask = cv.inRange(hsv_image, LOWER_BOUND_COLOR, UPPER_BOUND_COLOR)
+    mask = cv.inRange(
+        hsv_image,
+        np.array([config.getint("LOWER_HUE"), config.getint("LOWER_SATURATION"), config.getint("LOWER_VALUE")]),
+        np.array([config.getint("UPPER_HUE"), config.getint("UPPER_SATURATION"), config.getint("UPPER_VALUE")]),
+    )
 
     # Remove tiny contours on the mask to make it more clear.
     mask = cv.erode(mask, None, iterations=2)
@@ -44,11 +55,13 @@ def detect_object(image: np.ndarray) -> int:
     contours_list = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours_list = imutils.grab_contours(contours_list)
 
-    if len(contours_list) > 0:
+    if contours_list:
         selected_contour = choose_contour(contours_list)
-        ((_, _), radius) = cv.minEnclosingCircle(selected_contour)
-        return int(radius)
-    return 0
+        ((x, y), radius) = cv.minEnclosingCircle(selected_contour)
+
+        # Times 2, because the diameter is returned
+        return ((int(x), int(y)), int(2 * radius))
+    return ((-1, -1), -1)
 
 
 def choose_contour(contours_list: list) -> np.ndarray:
@@ -58,15 +71,14 @@ def choose_contour(contours_list: list) -> np.ndarray:
 
     Parameters
     ----------
-    contours_list : list
+    contours_list: list
         List of contours, from which the contour of object to be detected, will be seleted.
 
     Returns
     -------
-    np.ndarray
+    selected_contour: np.ndarray
         Contour of detected object.
     """
-
     max_criteria = 0
 
     for contour in contours_list:
@@ -79,4 +91,5 @@ def choose_contour(contours_list: list) -> np.ndarray:
         if circularity * area > max_criteria:
             max_criteria = circularity * area
             selected_contour = contour
+
     return selected_contour
